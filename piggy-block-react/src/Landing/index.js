c61aaf2c9b760054e20ebd178a6c0301d99ff338
okekeobasi <okekeobasi@gmail.com>
2018-10-06 00:22:52 +0100

    Fixed the NavBar, Header and Landing page bugs

--- piggy-block-react/src/Landing/index.js
+++ piggy-block-react/src/Landing/index.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import Body from './../Components/Body';
 import Header from './../Components/Header';
 import { Grid } from 'react-bootstrap';
+import NavBar from './../Components/Navbar';
 
 class Landing extends Component{
     render(){
         return(
-            <Grid>
-                <Header />
-                <Body />
-            </Grid>
+            <div>
+                <NavBar />
+                <Grid>
+                    <Header />
+                    <Body />
+                </Grid>
+            </div>
         );
     }
 }
